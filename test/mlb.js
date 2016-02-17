4664d930d8756d4b93131f45ed7b7a376f231759
James Weston <jawone87@gmail.com>
2016-02-16 21:41:13 -0500

    FIRST

---
+++ test/mlb.js
@@ -0,0 +1,40 @@
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+
+var Mlb = require('../');
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('Plays', function () {
+
+    it('Can get boxscores', function (done) {
+
+        var options = {
+            path: 'year_2011/month_07/day_23/'
+        };
+
+        var mlb = new Mlb(options);
+
+        mlb.get(function (err, boxscores) {
+
+            expect(err).to.not.exist();
+
+            expect(boxscores).to.exist();
+            expect(boxscores.length).to.exist();
+
+            done();
+        });
+    });
+});
